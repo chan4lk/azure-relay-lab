9ddf0cf037c334e432476a8aa132f24991703834
ChandimaRanaweera <ChandimaRanaweera@DESKTOP-C9PKMBU>
2023-12-28 10:51:27 +0530

    Add project files.

---
+++ Lab.Relay.Client/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab.Relay.Client;
+using Microsoft.IdentityModel.Tokens;
+
+Console.WriteLine("Hello, World!");
+
+await new RelayClient().RunAsync();
---
+++ Lab.Relay.Server/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab.Relay.Server;
+
+Console.WriteLine("Hello, World!");
+
+await new RelayServer().RunAsync();
